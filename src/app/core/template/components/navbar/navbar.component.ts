216de88f8f7d11deb274d72382d03a70416bc1d9
moumouhr <m82.ramdani@gmail.com>
2022-11-26 19:53:20 +0100

    Ajout de l'option Save All

--- src/app/core/template/components/navbar/navbar.component.ts
+++ src/app/core/template/components/navbar/navbar.component.ts
@@ -1,83 +1,83 @@
-import {Component, ElementRef, OnInit} from '@angular/core';
-import {ROUTES} from "../sidebar/sidebar.component"
-import {Location} from '@angular/common';
-import {OAuthService} from "angular-oauth2-oidc";
-import {filter} from "rxjs/operators";
+import { Component, ElementRef, OnInit } from "@angular/core";
+import { ROUTES } from "../sidebar/sidebar.component";
+import { Location } from "@angular/common";
+import { OAuthService } from "angular-oauth2-oidc";
+import { filter } from "rxjs/operators";
 
 @Component({
-    selector: 'navbar-cmp',
-    templateUrl: 'navbar.component.html'
+  selector: "navbar-cmp",
+  templateUrl: "navbar.component.html",
 })
-
 export class NavbarComponent implements OnInit {
-    private listTitles: any[];
-    location: Location;
-    private toggleButton: any;
-    private sidebarVisible: boolean;
-    public name: string;
+  private listTitles: any[];
+  location: Location;
+  private toggleButton: any;
+  private sidebarVisible: boolean;
+  public name: string;
 
+  constructor(
+    location: Location,
+    private element: ElementRef,
+    private oauthService: OAuthService
+  ) {
+    this.location = location;
+    this.sidebarVisible = false;
+  }
 
-    constructor(location: Location, private element: ElementRef, private oauthService: OAuthService) {
-        this.location = location;
-        this.sidebarVisible = false;
+  ngOnInit() {
+    this.listTitles = ROUTES.filter((listTitle) => listTitle);
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName("navbar-toggle")[0];
+    this.oauthService.events
+      .pipe(filter((e) => e.type === "token_received"))
+      .subscribe((_) => {
+        this.name = this.oauthService.getIdentityClaims()["name"];
+      });
+  }
+
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const body = document.getElementsByTagName("body")[0];
+    setTimeout(function () {
+      toggleButton.classList.add("toggled");
+    }, 500);
+    body.classList.add("nav-open");
+
+    this.sidebarVisible = true;
+  }
+
+  sidebarClose() {
+    const body = document.getElementsByTagName("body")[0];
+    this.toggleButton.classList.remove("toggled");
+    this.sidebarVisible = false;
+    body.classList.remove("nav-open");
+  }
+
+  sidebarToggle() {
+    // const toggleButton = this.toggleButton;
+    // const body = document.getElementsByTagName('body')[0];
+    if (this.sidebarVisible === false) {
+      this.sidebarOpen();
+    } else {
+      this.sidebarClose();
+    }
+  }
+
+  getTitle() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === "#") {
+      titlee = titlee.slice(1);
     }
 
-    ngOnInit() {
-        this.listTitles = ROUTES.filter(listTitle => listTitle);
-        const navbar: HTMLElement = this.element.nativeElement;
-        this.toggleButton = navbar.getElementsByClassName('navbar-toggle')[0];
-        this.oauthService.events
-            .pipe(filter(e => e.type === 'token_received'))
-            .subscribe(_ => {
-                console.log(this.oauthService.getIdentityClaims());
-                this.name = this.oauthService.getIdentityClaims()['name'];
-            });
-
+    for (var item = 0; item < this.listTitles.length; item++) {
+      if (this.listTitles[item].path === titlee) {
+        return this.listTitles[item].title;
+      }
     }
+    return "";
+  }
 
-    sidebarOpen() {
-        const toggleButton = this.toggleButton;
-        const body = document.getElementsByTagName('body')[0];
-        setTimeout(function () {
-            toggleButton.classList.add('toggled');
-        }, 500);
-        body.classList.add('nav-open');
-
-        this.sidebarVisible = true;
-    };
-
-    sidebarClose() {
-        const body = document.getElementsByTagName('body')[0];
-        this.toggleButton.classList.remove('toggled');
-        this.sidebarVisible = false;
-        body.classList.remove('nav-open');
-    };
-
-    sidebarToggle() {
-        // const toggleButton = this.toggleButton;
-        // const body = document.getElementsByTagName('body')[0];
-        if (this.sidebarVisible === false) {
-            this.sidebarOpen();
-        } else {
-            this.sidebarClose();
-        }
-    };
-
-    getTitle() {
-        var titlee = this.location.prepareExternalUrl(this.location.path());
-        if (titlee.charAt(0) === '#') {
-            titlee = titlee.slice(1);
-        }
-
-        for (var item = 0; item < this.listTitles.length; item++) {
-            if (this.listTitles[item].path === titlee) {
-                return this.listTitles[item].title;
-            }
-        }
-        return '';
-    }
-
-    logout() {
-        this.oauthService.logOut();
-    }
+  logout() {
+    this.oauthService.logOut();
+  }
 }
